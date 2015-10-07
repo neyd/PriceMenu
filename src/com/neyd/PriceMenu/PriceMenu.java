3cd2cb5fbc104bdce14ad12799de629c3b515685
neyd <jev.neydel@gmail.com>
2015-10-07 21:41:14 +0300

    Рівень перший

---
+++ src/com/neyd/PriceMenu/PriceMenu.java
@@ -0,0 +1,23 @@
+package com.neyd.PriceMenu;
+
+/**
+ * Created by Женя on 07.10.2015.
+ */
+public class PriceMenu {
+    public static void main(String[] args) {
+
+        Dash[] dashs = new Dash[5];
+        dashs[0] = new Dash(13, "Гречка");
+        dashs[1] = new Dash(8, "Підлива з м'ясом");
+        dashs[2] = new Dash(15, "Картошка");
+        dashs[3] = new Dash(13, "Відбивна");
+        dashs[4] = new Dash(6, "Компот");
+
+
+        // Вивести всі пропозиції
+        for (int i = 0; i < dashs.length; i++) {
+            System.out.println(dashs[i]);
+
+        }
+    }
+}
